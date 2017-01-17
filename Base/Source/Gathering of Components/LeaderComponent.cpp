ceaa00f85d1b0d8f248ec3edf834a0dd337ad1ba
Ah Heng <leesekheng@yahoo.com>
2017-01-17 17:11:53 +0800

    Thinking of how to make the leader component more productive.

--- Base/Source/Gathering of Components/LeaderComponent.cpp
+++ Base/Source/Gathering of Components/LeaderComponent.cpp
@@ -3,21 +3,38 @@
 LeaderComponent::LeaderComponent()
 {
     name_ = "Leader Component";
+    changeLeaderName = false;
+    originalName = "";
 }
 
 LeaderComponent::~LeaderComponent()
 {
+    Exit();
+}
 
+void LeaderComponent::Init()
+{
+    changeLeaderName = false;
+    originalName = "";
+}
+
+void LeaderComponent::Update(double dt)
+{
+
+}
+
+void LeaderComponent::Exit()
+{
+    if (changeLeaderName)   // check whether the name has been changed before. If so, change it back!
+    {
+        owner_of_component->setName(originalName);
+        changeLeaderName = false;
+        originalName = "";
+    }
 }
 
 bool LeaderComponent::onNotify(GenericEntity &zeEvent)
 {
-    //allOfItsFollowers.push_back(dynamic_cast<GameEntity*>(&zeEvent));
-    allOfItsFollowers.insert(std::pair<std::string, GameEntity*>(zeEvent.getName(), dynamic_cast<GameEntity*>(&zeEvent)));
     return true;
 }
 
-GameEntity *LeaderComponent::getFollower(const std::string &zeName)
-{
-    return allOfItsFollowers[zeName];
-}
--- Base/Source/Gathering of Components/LeaderComponent.h
+++ Base/Source/Gathering of Components/LeaderComponent.h
@@ -10,14 +10,16 @@ public:
     LeaderComponent();
     virtual ~LeaderComponent();
 
-    // This is just for adding the all the followers into it's map using their name as the key. For now it is useless.....
+    virtual void Init();
+    virtual void Update(double dt);
+    virtual void Exit();
+
     virtual bool onNotify(GenericEntity &zeEvent);
-    GameEntity *getFollower(const std::string &zeName);
-    // This is just for adding the all the followers into it's map using their name as the key. For now it is useless.....
 
     static const unsigned char ID_ = 11;
 
 private:
-    // using unordered map so it can access the string faster.
-    std::unordered_map<std::string, GameEntity*> allOfItsFollowers;
+
+    bool changeLeaderName;  // Need to check whether it has change any name
+    std::string originalName;   // Able to retain the entity original name
 };
